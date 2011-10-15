d9ee580a7c042b7c6e43a8173e6ccda3669b4a0c
Paulo Ragonha <paulo@pirelenito.org>
2011-10-15 02:59:03 -0300

    clean Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clipmeme::Application.config.secret_token = '8bac1275d6fdd33fe2337c2320d461d7510a9b2fad811852a58fbd198a9034cdb39209d7bed0c0684c37b03700b3b8e871690b372b0fe8c6d5691d38f9c86aab'
